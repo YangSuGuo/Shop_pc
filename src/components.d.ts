ac80abde1a3579338dddbbcab0766176b7ac0cda
PMJ <1156112589@qq.com>
2024-03-21 13:55:33 +0800

    测试

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,37 +7,18 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    Aside: typeof import('./components/Aside.vue')['default']
     ElAside: typeof import('element-plus/es')['ElAside']
     ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElForm: typeof import('element-plus/es')['ElForm']
-    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElLink: typeof import('element-plus/es')['ElLink']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
-    Header: typeof import('./components/Header.vue')['default']
-    Home: typeof import('./components/home.vue')['default']
-    IEpChatRound: typeof import('~icons/ep/chat-round')['default']
-    IEpIcon: typeof import('~icons/ep/icon')['default']
-    IEpPlus: typeof import('~icons/ep/plus')['default']
-    IEpSwitchButton: typeof import('~icons/ep/switch-button')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Sidebar: typeof import('./components/components/sidebar.vue')['default']
-    TableDetail: typeof import('./components/components/table-detail.vue')['default']
-    TableEdit: typeof import('./components/components/table-edit.vue')['default']
-    Tags: typeof import('./components/components/tags.vue')['default']
-  }
-  export interface ComponentCustomProperties {
-    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
   }
 }
